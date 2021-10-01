4415fa9c4e71259b74a234ec10bfe3248240fb19
Hritik Kumar Singh <hritik.bnp@gmail.com>
2021-10-01 14:24:11 +0530

    .

---
+++ src/main/java/com/example/springbootwebsocketchat/SpringBootWebsocketChatApplication.java
@@ -0,0 +1,15 @@
+package com.example.springbootwebsocketchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+@SpringBootApplication
+public class SpringBootWebsocketChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebsocketChatApplication.class, args);
+	}
+
+}
